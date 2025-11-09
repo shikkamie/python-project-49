1dc9c098a67047ce5e0c74a9b22a0ea9f382df4a
shikkamie <shikamie@yandex.ru>
2025-11-09 20:31:40 +0300

    1 игра Загрузка записи терминала + readme

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,32 @@
+import random
+import prompt
+
+
+def find_even(name="Bill"):
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    count_answers = 0
+    while count_answers < 3:
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+        answer = input("Your answer: ")
+        if (number % 2 == 0 and answer == "yes") or (
+            number % 2 != 0 and answer == "no"
+        ):
+            print("Correct!")
+            count_answers += 1
+        else:
+            correct_answer = "yes" if number % 2 == 0 else "no"
+            print(
+                f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'."
+            )
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    find_even(name)
